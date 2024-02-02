6fe8d2d6e0f584eb764b881bfcea355a0955f857
Kirill0170 <kirill17.rzd@icloud.com>
2024-02-02 09:19:15 +0300

    update session

--- Global/session.lua
+++ Global/session.lua
@@ -7,8 +7,8 @@ function session.checkSession(sessionInfo)
     if not ip.isUUID(sessionInfo["uuid"]) then return false end
     if not ip.isIPv2(sessionInfo[0]) then return false end
     if not tonumber(sessionInfo["ttl"]) then return false end
-    if not tonumber(sessionInfo["c"]) then return false end
-    if not ip.isIPv2(sessionInfo["t"]) and sessionInfo["t"]~="broadcast" then return false end
+    if not sessionInfo["route"] then return false end
+    if not ip.isIPv2(sessionInfo["t"]) and sessionInfo["t"]~="broadcast" and sessionInfo["t"]~="dns_lookup" then return false end
     return true
 end
 function session.newSession(from_ip,to_ip,ttl)
@@ -17,17 +17,23 @@ function session.newSession(from_ip,to_ip,ttl)
     if not tonumber(ttl) then ttl=16 end
     local newSession={}
     newSession["uuid"]=require("uuid").next()
-    newSession["c"]=1
     newSession["t"]=to_ip
-    newSession[0]=from_ip
+    newSession["route"]={}
+    newSession["route"][0]=from_ip
     newSession["ttl"]=tonumber(ttl)
     return newSession
 end
 function session.addIpToSession(sessionInfo,ip_a)
     if not session.checkSession(sessionInfo) then error("[SESSION ADD]: Invalid Session") end
     if not ip.isIPv2(ip_a) then error("[SESSION ADD]: Not an IP") end
-    sessionInfo[sessionInfo["c"]]=ip_a
-    sessionInfo["c"]=sessionInfo["c"]+1
+    sessionInfo["route"][#sessionInfo["route"]+1]=ip_a
     return sessionInfo
+end
+function session.adjustPattern(sessionInfo,ttl,reverse)
+    if not session.checkSession(sessionInfo) then return false end
+    if not reverse then reverse=false end
+    if not ttl then ttl=16 end
+    sessionInfo["ttl"]=ttl
+    --indev
 end
 return session
