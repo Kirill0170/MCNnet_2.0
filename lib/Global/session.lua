4238d06477c56da41f578e6d5ea13aff83bb1427
Kirill0170 <kirill17.rzd@icloud.com>
2024-02-06 21:25:46 +0300

    debug+fixes

--- lib/Global/session.lua
+++ lib/Global/session.lua
@@ -1,18 +1,31 @@
 local ip=require("ipv2")
+local computer=require("computer")
 local version="1.4 EXPERIMENTAL"
 local session={}
+local dolog=true
+local function log(text)
+    local res="["..computer.uptime().."]"
+    if dolog then print(res.."[SS/INFO]"..text) end
+end
 function session.ver() return version end
-function session.checkSession(sessionInfo)
-    if not sessionInfo then return false end
-    if not ip.isUUID(sessionInfo["uuid"]) then return false end
-    if not sessionInfo["route"] then return false end
-    if not ip.isIPv2(sessionInfo["route"][0]) then return false end
-    if not tonumber(sessionInfo["ttl"]) then return false end
-    if not ip.isIPv2(sessionInfo["t"]) and sessionInfo["t"]~="broadcast" and sessionInfo["t"]~="dns_lookup" then return false end
+function session.checkSession(sessionInfo) --log for debug
+    if not sessionInfo then log("nothing was given") return false end
+    if not ip.isUUID(sessionInfo["uuid"]) then log("no session uuid") return false end
+    if not sessionInfo["route"] then log("no route table") return false end
+    if not ip.isIPv2(sessionInfo["route"][0]) then log("no original ip") return false end
+    if not tonumber(sessionInfo["ttl"]) then log("no ttl") return false end
+    if not ip.isIPv2(sessionInfo["t"]) and sessionInfo["t"]~="broadcast" and sessionInfo["t"]~="dns_lookup" then 
+        if sessionInfo["t"] then log("invalid destination: "..sessionInfo["t"]) else log("no destination") end
+        return false 
+    end
     return true
 end
 function session.newSession(from_ip,to_ip,ttl)
-    if not ip.isIPv2(from_ip) then return nil end
+    if not ip.isIPv2(from_ip) then
+        if ip.isIPv2(os.getenv("this_ip")) then --try to use default
+            from_ip=os.getenv("this_ip")
+        else return nil end
+    end
     if to_ip=="dns_lookup" then --pass
     elseif not ip.isIPv2(to_ip) or not to_ip then to_ip="broadcast" end
     if not tonumber(ttl) then ttl=16 end
