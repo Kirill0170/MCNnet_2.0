03d3b1f15cd2e352917f27892c6d803166e94581
Kirill0170 <kirill17.rzd@icloud.com>
2024-10-25 22:54:04 +0300

    session rewrite

--- Global/lib/session.lua
+++ Global/lib/session.lua
@@ -1,18 +1,27 @@
 local ip=require("ipv2")
 local computer=require("computer")
-local version="1.42 EXPERIMENTAL"
+local version="1.52 EXPERIMENTAL"
 local session={}
 local dolog=true
-local function log(text)
+function log(text)
     local res="["..computer.uptime().."]"
     if dolog then print(res.."[SS/INFO]"..text) end
 end
 function session.ver() return version end
+function session.checkRoute(route)
+    if type(route)~="table" then return false end
+    local length=0
+    for _ in pairs(route) do length=length+1 end
+    for i=0,length-1 do
+        if not ip.isIPv2(route[i]) then return false end
+    end
+    return true
+end
 function session.checkSession(sessionInfo) --log for debug
     if not sessionInfo then log("nothing was given") return false end
     if not ip.isUUID(sessionInfo["uuid"]) then log("no session uuid") return false end
     if not sessionInfo["route"] then log("no route table") return false end
-    if not ip.isIPv2(sessionInfo["route"][0]) then log("no original ip") return false end
+    if not session.checkRoute(sessionInfo["route"]) then log("incorrect route") return false end
     if not tonumber(sessionInfo["ttl"]) then log("no ttl") return false end
     if not ip.isIPv2(sessionInfo["t"]) and sessionInfo["t"]~="broadcast" and sessionInfo["t"]~="dns_lookup" then 
         if sessionInfo["t"] then log("invalid destination: "..sessionInfo["t"]) else log("no destination") end
@@ -21,6 +30,7 @@ function session.checkSession(sessionInfo) --log for debug
     return true
 end
 function session.newSession(to_ip,ttl)
+    from_ip=""
     if ip.isIPv2(os.getenv("this_ip")) then --try to use default
         from_ip=os.getenv("this_ip")
     else return nil end
@@ -40,12 +50,5 @@ function session.addIpToSession(sessionInfo,ip_a)
     if not ip.isIPv2(ip_a) then error("[SESSION ADD]: Not an IP") end
     sessionInfo["route"][#sessionInfo["route"]+1]=ip_a
     return sessionInfo
-end
-function session.adjustPattern(sessionInfo,ttl,reverse)
-    if not session.checkSession(sessionInfo) then return false end
-    if not reverse then reverse=false end
-    if not ttl then ttl=16 end
-    sessionInfo["ttl"]=ttl
-    --indev
 end
 return session
