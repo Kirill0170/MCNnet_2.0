c11d16bfb73350a45602973343474cb382144d7e
Kirill0170 <kirill17.rzd@icloud.com>
2024-11-29 23:00:06 +0300

    enable c2c pinging for ssap servers

--- Client/bin/ssapServer.lua
+++ Client/bin/ssapServer.lua
@@ -1,5 +1,7 @@
 local application="ssap_application_template" --example
 local ssap=require("ssap")
+local cmnp=require("cmnp")
+require("thread").create(cmnp.mncp.c2cPingService):detach()
 if not ssap.serverConnectionManager(application) then
-    print("An error occured and server was stopped")
+    print("Server was stopped")
 end
