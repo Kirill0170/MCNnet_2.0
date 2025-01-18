2ff8f33ab213be58aa3fef01ad4f25a5fcfdd69f
Kirill0170 <kirill17.rzd@icloud.com>
2025-01-18 16:44:08 +0300

    update/install initial

--- Dev/apm/usr/bin/apm.lua
+++ Dev/apm/usr/bin/apm.lua
@@ -1,4 +1,4 @@
-local ver = "0.0"
+local ver = "0.1.0"
 local sources_file = "/etc/apm-sources.st" --serialized table with sources
 local default_source_server = "pkg.com" --default source list server
 local sources = {} --sources[pname]={server,latest_version, installed_version}
@@ -7,6 +7,7 @@ local shell = require("shell")
 local ip = require("ipv2")
 local apm = require("apm-lib")
 local component = require("component")
+local computer=require("computer")
 local ser = require("serialization")
 local gpu = component.gpu
 
@@ -14,6 +15,16 @@ local function cprint(text, color)
 	gpu.setForeground(color)
 	print(text)
 	gpu.setForeground(0xFFFFFF)
+end
+
+local function ftime(sec)
+  if sec>60 then
+    local mins=sec//60
+    sec=sec%60
+    return mins.."m"..sec.."s"
+  else
+    return sec.."s"
+  end
 end
 
 --functions
@@ -28,6 +39,7 @@ local function help()
 	print("             addsrc   [hostname]")
 	print("             listsrc")
 	print("             rmsrc    [hostname]")
+  print("             fetchsrc")
 	print("             update")
 	print("             upgrade  <pname>")
 	cprint("Options:", 0x33CC33)
@@ -79,20 +91,64 @@ function connection(dest, timeout)
 		print("Connecting to " .. to_ip)
 	end
 end
-function update()
-	cprint(">>Updating package list", 0x6699FF)
-
-	--get sources from default_source_server
-	local check, to_ip = mnp.checkAvailability(default_source_server)
+function fetchDefaultSources()
+  local check, to_ip = mnp.checkAvailability(default_source_server)
 	if not check then
 		cprint("!!Error: Couldn't connect to default source server!", 0xFF0000)
-		return false
+		return nil
 	else
 		cprint(">>Connecting to " .. to_ip, 0xFFFF33)
 		local default_sources = apm.getDefaultSources(to_ip)
+    if default_sources then
+      cprint(">>Successfully fetched default sources list ("..#default_sources.."entries)",0x33CC33)
+      os.sleep(0.2)
+      cprint(">>Setting up..",0xFFFF33)
+      os.sleep(0.2)
+      for i=1,#default_sources do
+        sources[default_sources[i][1]]=default_sources[i][2]
+        print("["..default_sources[i][2][1]"] "..default_sources[i][1].." "..default_sources[i][2][2])
+      end
+    else
+      cprint("!!Error: Failed to fetch default sources!",0xFF0000)
+      return false
+    end
 	end
 end
-function install(pname)
+function update()
+	cprint(">>Updating package list", 0x6699FF)
+  --collect
+  cprint(">>Collecting servers..",0xFFFF33)
+  local queue={}
+  local len=0
+  for pname,pinfo in pairs(sources) do
+    if not queue[pinfo[1]] then
+      queue[pinfo[1]]={pname}
+      len=len+1
+    else
+      table.insert(queue[pinfo[1]],pname)
+    end
+  end
+  print("--Collected "..len.." servers!")
+  local start_time=computer.uptime()
+  for server,pnames in pairs(queue) do
+    local check,server_ip=mnp.checkAvailability(server)
+    if not check then
+      cprint("!!Error: Couldn't connect to "..server.."! Skipping all packages from it.",0xFF0000)
+    else
+      for _,pname in pairs(pnames) do
+        local latest_ver=apm.getInfo(server,pname)
+        if sources[pname][2]~=latest_ver then
+          cprint(">>New version: "..pname.." "..sources[pname][2].." -> "..latest_ver,0x6699FF)
+          sources[pname][2]=latest_ver
+        else
+          cprint(">>Not changed: "..pname.." "..latest_ver)
+        end
+      end
+    end
+  end
+  print(">>Completed, took "..ftime(computer.uptime()-start_time))
+end
+function install(pname,force)
 	if not mnp.isConnected() then
 		cprint("!!Error: You should be connected to network!", 0xFF0000)
 		return false
@@ -113,13 +169,16 @@ function install(pname)
 		return false
 	end
 	cprint(">>Connecting to " .. to_ip, 0xFFFF33)
-	local success, err = apm.getPackage(to_ip, pname, true)
+  local start_time=computer.uptime()
+	local success, err = apm.getPackage(to_ip, pname, true,sources[pname][3],force)
 	if not success then
 		if err == "aborted" then
 			cprint("Aborted")
 			return
 		end
 		cprint("!!Error: Couldn't get packet: " .. err, 0xFF0000)
+  else
+    cprint(">>Installed successfully! Took "..ftime(computer.uptime()-start_time),0x33CC33)
 	end
 end
 --main
@@ -137,8 +196,9 @@ if not args and not ops then
 	help()
 elseif ops["h"] or ops["help"] then
 	help()
-elseif args[1] == "install" then
-	install(args[2])
+elseif args[1]=="install" then install(args[2],ops["f"])
+elseif args[1]=="update" then update()
+elseif args[1]=="fetchsrc" then fetchDefaultSources()
 else
 	help()
 end
