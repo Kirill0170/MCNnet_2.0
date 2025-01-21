52678bbc3c2aa4388c51eb7abacbb5098ced25ff
Kirill0170 <kirill17.rzd@icloud.com>
2025-01-21 10:11:27 +0300

    ipv2.4 (untested)

--- Global/usr/lib/ipv2.lua
+++ Global/usr/lib/ipv2.lua
@@ -5,22 +5,38 @@
 local component=require("component")
 if not component.isAvailable("modem") then error("[IP INIT]: No modem present") end
 local this_uuid=component.getPrimary("modem")["address"]
-local this_ip=os.getenv("this_ip")
-local ip_ver="2.3 BETA"
+local ip_ver="2.4 BETA"
 local nips={} --nips[<ip>]=<uuid>
 local ip={}
 ----------------------------
 function ip.ver() return ip_ver end
 
+function ip.getParts(g_ip)
+  if not g_ip then return nil end
+  if not string.find( g_ip,":") then return nil end
+  local a=string.sub(g_ip,1,string.find(g_ip,":")-1)
+  local b=string.sub(g_ip,string.find(g_ip,":")+1)
+  return a,b
+end
+
 function ip.isIPv2(g_ip,nodechk)--nodechk for checking if node
-  if not g_ip then return false end
+  if not g_ip then return false,nil end
   local pt="^%x%x%x%x:%x%x%x%x$"
   local pt2="^%x%x%x%x:0000$"
+  local pt3="^:%x%x%x%x$"
   if nodechk then
-    if string.match(g_ip,pt2) then return true
+    if string.match(g_ip,pt2) then return true,g_ip
     else return false end
   end
-  if string.match(g_ip,pt) then return true end
+  local this_ip=os.getenv("this_ip")
+  if not this_ip then this_ip="" end
+  if string.match(this_ip,pt) then --if connected, check local
+    if string.match(g_ip,pt3) then --make ipv2
+      local a=ip.getParts(this_ip)
+      return true,a..g_ip
+    end
+  end
+  if string.match(g_ip,pt) then return true,g_ip end
   return false
 end
 
@@ -29,10 +45,6 @@ function ip.isUUID(g_uuid) --rewrite
   local pattern = "^%x%x%x%x%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%x%x%x%x%x%x%x%x$"
   if string.match(g_uuid, pattern) then return true
   else return false end
-end
-
-function ip.gnip()
-  return string.sub(this_uuid,1,4)..":0000"
 end
 
 function ip.set(g_ip,node)
@@ -52,13 +64,11 @@ function ip.remove()
   os.setenv("this_ip",nil)
 end
 
-function ip.getParts(g_ip)
-  if not ip.isIPv2(g_ip) then return nil end
-  local a=string.sub(g_ip,1,string.find(g_ip,":")-1)
-  local b=string.sub(g_ip,string.find(g_ip,":")+1)
-  return a,b
-end
 ---Node IPs (nips)-----------------
+function ip.gnip()
+  return string.sub(this_uuid,1,4)..":0000"
+end
+
 function ip.fromUUID(g_uuid)
   return string.sub(this_uuid,1,4)..":"..string.sub(g_uuid,1,4)
 end
@@ -99,8 +109,8 @@ end
 
 function ip.deleteUUID(g_uuid)
   for n_ip,n_uuid in pairs(nips) do
-    if n_uuid==g_uuid then 
-      nips[n_ip]=nil 
+    if n_uuid==g_uuid then
+      nips[n_ip]=nil
       return n_ip
     end
   end
